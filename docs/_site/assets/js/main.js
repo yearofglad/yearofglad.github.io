dfd6687412c316efc8e8e28180fa826329e101e2
Ben Fu <bhf2117@columbia.edu>
2025-01-04 02:15:00 -0500

    Modified how website scrolls

---
+++ docs/_site/assets/js/main.js
@@ -0,0 +1,11 @@
+var btnContainer = document.getElementById("navigator");
+
+var btns = btnContainer.getElementsByClassName("nav-item");
+
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+} 
